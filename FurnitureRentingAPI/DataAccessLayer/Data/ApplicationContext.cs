8f9d35c96c29bb0941cc0d57c023d93a4d3fcf47
Nirali <chitrodanirali90@gmail.com>
2024-06-29 13:05:37 +0530

    Nirali - Added Context and JWT

---
+++ FurnitureRentingAPI/DataAccessLayer/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Data
+{
+    public class ApplicationContext : DbContext
+    { 
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+             : base(options)
+        {
+        }
+
+        protected ApplicationContext()
+        { }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        //    modelBuilder.ApplyConfiguration(new UserConfiguration());
+        //}
+
+        //public DbSet<Products> Products { get; set; }
+
+        //public DbSet<Users> Users { get; set; }
+    }
+}
