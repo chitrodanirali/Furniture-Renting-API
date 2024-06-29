b93fc6805c7bbe637fd544ed4bb32eb2187682d9
Nirali <chitrodanirali90@gmail.com>
2024-06-29 14:06:37 +0530

    Added auth .

---
+++ FurnitureRentingAPI/FurnitureRentingAPI/Controllers/AuthorizedController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureRentingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Authorized : ControllerBase
+    {
+    }
+}
