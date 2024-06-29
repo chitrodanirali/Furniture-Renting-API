3ec65eb4dfc6d97c7f6085aabf77531f46629a4c
Nirali <chitrodanirali90@gmail.com>
2024-06-29 14:09:58 +0530

    Test commit

--- FurnitureRentingAPI/FurnitureRentingAPI/Controllers/WeatherForecastController.cs
+++ FurnitureRentingAPI/FurnitureRentingAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FurnitureRentingAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracidng", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
